de6c5db33b49e1640d6e501b61429eb5f1e314be
IT21162978 <99311547+IT21162978-Dinithi@users.noreply.github.com>
2023-05-02 19:49:43 +0530

    Add booking page and booking details retrieve page

---
+++ app/src/main/java/com/example/mad_miniproject/BookingDetailedActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mad_miniproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BookingDetailedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_booking_detailed)
+    }
+}
