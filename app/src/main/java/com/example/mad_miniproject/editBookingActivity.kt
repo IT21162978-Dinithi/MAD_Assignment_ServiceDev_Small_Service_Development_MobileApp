de64ffdab17df008b9bae84d2e66b759e7530384
IT21162978 <99311547+IT21162978-Dinithi@users.noreply.github.com>
2023-05-02 19:57:33 +0530

    Add edit booking page

---
+++ app/src/main/java/com/example/mad_miniproject/editBookingActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mad_miniproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class editBookingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_booking)
+    }
+}
